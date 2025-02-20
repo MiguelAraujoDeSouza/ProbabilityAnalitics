35f84eaac3b4610355810b494a6c3df3917073a2
MiguelAraujoDeSouza <miguel0araujo0souza@gmail.com>
2025-02-20 18:45:35 -0300

    fix : Ajusting the menu

--- main.py
+++ main.py
@@ -51,34 +51,38 @@ def choosingRecomended(products: dict) -> dict:
      
 
 def menu():
-    print("\nBem-vindo à loja! Escolha um ou mais produtos para adicionar ao carrinho.")
-    excel = pd.read_excel('./loja.xlsx')
-    categorias = excel['Sub-Category'].unique()  # Pega as categorias únicas
-    print("Escolha uma opção: \n")
-    for i, categoria in enumerate(categorias, start=1):
-        print(f"{i} - {categoria}")
-    while True:
-        print("\n\n1 - Adicionar produto no carrinho: ")
-        print("2 - não comprar nada ")
-        print("3 - sair da loja \n")
-        opcao = int(input("Digite sua opção: "))
-        if(opcao == 1):
-            carrinho = str(input("\n\ndigite o seu carrinho"))
-            print(carrinho)
-            array = carrinho.split(", ")
-            produtoo = recommendation(array)
-            proximo_produto = choosingRecomended(produtoo)
-            print(proximo_produto)
-        elif(opcao == 2):
-            print("não comprar nada, suas recomendaçoes iniciais serão: ")
-            produtoo = recommendation([])
-            proximo_produto = choosingRecomended(produtoo)
-            print(proximo_produto)
-        elif(opcao == 3):
-            print("Saindo...")
-            break
-        else:
-            print("de 1 a 2")
+     print("\nBem-vindo à loja! Escolha um ou mais produtos para adicionar ao carrinho.")
+     excel = pd.read_excel('./loja.xlsx')
+     categorias = excel['Sub-Category'].unique()  # Pega as categorias únicas
+     print("Essas são suas opções: \n")
+     for i, categoria in enumerate(categorias, start=1):
+          print(f"{categoria}")
+     while True:
+          print("\n\n1 - Coloque o seu carrinho para receber uma recomendação")
+          print("2 - Quer uma recomendação inicial? ")
+          print("3 - sair da loja \n")
+          opcao = int(input("Digite sua opção: "))
+          if(opcao == 1):
+               carrinho = str(input("\n\ndigite o nome dos produtos no seu carrinho (separados por virgula): "))
+               print(carrinho)
+               array = carrinho.split(",")
+               produtoo = recommendation(array)
+               proximo_produto = choosingRecomended(produtoo)
+               print('----(Categoria - Probabilidade)----')
+               for i in proximo_produto:
+                    print(f"{i} -> {proximo_produto[i]}%")
+          elif(opcao == 2):
+               print("Suas recomendações são:\n")
+               produtoo = recommendation([])
+               proximo_produto = choosingRecomended(produtoo)
+               print('----(Categoria - Probabilidade)----')
+               for i in proximo_produto:
+                    print(f"{i} -> {proximo_produto[i]}%")
+          elif(opcao == 3):
+               print("Saindo...")
+               break
+          else:
+               print("de 1 a 3")
 
 
 
